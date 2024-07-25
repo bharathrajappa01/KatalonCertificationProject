b8411ef076bd671dd85a77284a5a8134283d73f6
Bharath Rajappa <bharath.rajappa@emeal.nttdata.com>
2024-07-25 13:04:15 +0100

    Renamed TSC

--- Test Suites/Collections/MyInfo_PersonalDetails_Attachments_TSC.ts
+++ Test Suites/Collections/MyInfo_PersonalDetails_Attachments_TSC.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>MyInfo_PersonalDetails_Attachments</name>
+   <name>MyInfo_PersonalDetails_Attachments_TSC</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
