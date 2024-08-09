70be29e7f7bae751a8412fe023c16b47ecae3cf1
Bharath Rajappa <bharath.rajappa@emeal.nttdata.com>
2024-08-09 10:28:56 +0100

    Refactor Test

--- Scripts/LoginPage TCs/Login_RecordMode_TC/Script1721212483063.groovy
+++ Scripts/LoginPage TCs/Login_RecordMode_TC/Script1721212483063.groovy
@@ -17,9 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 WebUI.maximizeWindow()
 
